a76541c0d5757066fdae4297a75572607cf1cc06
Areej Anjum <strugglebird99@gmail.com>
2021-12-17 18:05:12 +0500

    done

---
+++ components/Sidebar.js
@@ -0,0 +1,52 @@
+
+import { HeartIcon, SearchIcon, LibraryIcon, PlusCircleIcon, HomeIcon, RssIcon } from "@heroicons/react/outline"
+
+function Sidebar() {
+    return (
+        <div className="text-gray-500 p-2 text-sm border-r border-gray-900">
+            <div 
+            
+            >    
+                  <button className="flex items-center space-x-2 hover:text-white ">
+                <HomeIcon className="h-5 w-5"/>
+                <p>home</p>
+            </button>
+            
+            <button className="flex items-center space-x-2 hover:text-white ">
+                <SearchIcon className="h-5 w-5"/>
+                <p>Search</p>
+            </button>
+            
+            <button className="flex items-center space-x-2 hover:text-white ">
+                <LibraryIcon className="h-5 w-5"/>
+                <p>Your library</p>
+            </button>
+            <hr className="border-t-[1.08px] border-gray-900"/>
+            </div>
+           
+
+           <div 
+            
+            >    
+                  <button className="flex items-center space-x-2 hover:text-white ">
+                <PlusCircleIcon className="h-5 w-5"/>
+                <p>create playlist</p>
+            </button>
+            
+            <button className="flex items-center space-x-2 hover:text-white ">
+                <HeartIcon className="h-5 w-5"/>
+                <p>Liked songs</p>
+            </button>
+            
+            <button className="flex items-center space-x-2 hover:text-white ">
+                <RssIcon className="h-5 w-5"/>
+                <p>Your episodes</p>
+            </button>
+            <hr className="border-t-[1.08px] border-gray-900"/>
+            </div>
+
+        </div>
+    )
+}
+
+export default Sidebar
